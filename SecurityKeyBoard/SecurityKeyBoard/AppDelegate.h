e4ec5d71e999716a1a254ce912a136f58f6e17e1
zhongaibing <zhongaibing@sina.com>
2017-03-08 10:12:51 +0800

    上传项目

---
+++ SecurityKeyBoard/SecurityKeyBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecurityKeyBoard
+//
+//  Created by aibing zhong on 2017/3/8.
+//  Copyright © 2017年 aibing zhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecurityKeyBoard/SecurityKeyBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecurityKeyBoard
+//
+//  Created by aibing zhong on 2017/3/8.
+//  Copyright © 2017年 aibing zhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
