9444b35cbddbed3a84b9e2dfff68a954e683669f
zhongaibing <zhongaibing@sina.com>
2017-03-08 10:52:34 +0800

    添加指纹文件

---
+++ SecurityKeyBoard/Classes/TouchIDHandler.h
@@ -0,0 +1,13 @@
+//
+//  TouchIDHandler.h
+//  SecurityKeyBoard
+//
+//  Created by aibing zhong on 2017/3/8.
+//  Copyright © 2017年 aibing zhong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TouchIDHandler : NSObject
+
+@end
---
+++ SecurityKeyBoard/Classes/TouchIDHandler.m
@@ -0,0 +1,13 @@
+//
+//  TouchIDHandler.m
+//  SecurityKeyBoard
+//
+//  Created by aibing zhong on 2017/3/8.
+//  Copyright © 2017年 aibing zhong. All rights reserved.
+//
+
+#import "TouchIDHandler.h"
+
+@implementation TouchIDHandler
+
+@end
